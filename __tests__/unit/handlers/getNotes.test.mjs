b086a95e871d08f4447c301c902607c745232c9d
Matt Wilson <mawilson2012@gmail.com>
2024-04-08 20:59:00 -0700

    24 04 08 template changes (#1)
    * Including necessary files for SAM build
    
    * Updated handlers
    Updated handler names
    Updated /id endpoints to not provide id in body
    Added PUT & DELETE endpoints
    Updated template yaml for new endpoints
    
    * Corrected errors in handlers
    Fixed post & put note handlers
    Added test files
    Fixed gitignore for node modules
    Imported UUID NPM module
    Used UUID for POST ID
    
    * Updated readme with proper API use

---
+++ __tests__/unit/handlers/getNotes.test.mjs
@@ -0,0 +1,38 @@
+// Import getNotesHandler function from getNotes.mjs 
+import { getNotesHandler } from '../../../src/handlers/getNotes.mjs';
+// Import dynamodb from aws-sdk 
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { mockClient } from "aws-sdk-client-mock";
+ 
+// This includes all tests for getNotesHandler() 
+describe('Test getNotesHandler', () => { 
+    const ddbMock = mockClient(DynamoDBDocumentClient);
+ 
+    beforeEach(() => {
+        ddbMock.reset();
+      });
+ 
+    it('should return ids', async () => { 
+        const items = [{ id: 'id1' }, { id: 'id2' }]; 
+ 
+        // Return the specified value whenever the spied scan function is called 
+        ddbMock.on(ScanCommand).resolves({
+            Items: items,
+        }); 
+ 
+        const event = { 
+            httpMethod: 'GET' 
+        };
+ 
+        // Invoke helloFromLambdaHandler() 
+        const result = await getNotesHandler(event); 
+ 
+        const expectedResult = { 
+            statusCode: 200, 
+            body: JSON.stringify(items) 
+        }; 
+ 
+        // Compare the result with the expected result 
+        expect(result).toEqual(expectedResult); 
+    }); 
+}); 
---
+++ __tests__/unit/handlers/postNote.test.mjs
@@ -0,0 +1,40 @@
+// Import postNoteHandler function from postNote.mjs 
+import { postNoteHandler } from '../../../src/handlers/postNote.mjs';
+// Import dynamodb from aws-sdk 
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { mockClient } from "aws-sdk-client-mock";
+// This includes all tests for postNoteHandler() 
+describe('Test postNoteHandler', function () { 
+    const ddbMock = mockClient(DynamoDBDocumentClient);
+ 
+    beforeEach(() => {
+        ddbMock.reset();
+      });
+ 
+    // This test invokes postNoteHandler() and compare the result  
+    it('should add id to the table', async () => { 
+        const returnedItem = { id: 'id1', name: 'name1' }; 
+ 
+        // Return the specified value whenever the spied put function is called 
+        ddbMock.on(PutCommand).resolves({
+            returnedItem
+        }); 
+ 
+        const event = { 
+            httpMethod: 'POST', 
+            body: '{"id": "id1","name": "name1"}' 
+        }; 
+     
+        // Invoke postNoteHandler() 
+        const result = await postNoteHandler(event); 
+        
+        const expectedResult = { 
+            statusCode: 200, 
+            body: JSON.stringify(returnedItem) 
+        }; 
+ 
+        // Compare the result with the expected result 
+        expect(result).toEqual(expectedResult); 
+    }); 
+}); 
+ 
